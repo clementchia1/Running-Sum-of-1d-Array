ff1b64cd903dad5002b9226f1bece4cc6302a5cf
lemon <chiachingman1@gmail.com>
2021-05-04 23:51:23 +1000

    Add project files.

---
+++ Running Sum of 1d Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Running_Sum_of_1d_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] {1, 2, 3, 4, 5};
+            Program program = new Program();
+            Console.Write("RunningSum(");
+            Array.ForEach(nums, element=>Console.Write(element+ ", "));
+            Console.WriteLine("):");
+            Array.ForEach(program.RunningSum(nums), element => Console.Write(element + ", "));
+            
+        }
+        public int[] RunningSum(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+
+            result[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                result[i] = result[i - 1] + nums[i];
+            }
+            return result;
+        }
+    }
+}
